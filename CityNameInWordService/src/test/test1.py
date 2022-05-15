dbb60e94f073bea3c1adfcd9bfc07e505fc22b36
c101088 <15312731439@163.com>
2022-05-15 09:09:32 +0800

    修改命令行的启动内容

---
+++ CityNameInWordService/src/test/test1.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+# Author    : clz
+# Datetime  : 2021/8/28 21:10
+# User      : Administrator
+# Product   : PyCharm
+# Project   : CityNameInWordService
+# File      : test1.py
+# explain   : 文件说明
